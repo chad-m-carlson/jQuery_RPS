a16dc30400057ceb400eb87d9c42dbf2b30559a3
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-30 22:33:11 -0600

    responsive to screen size, fixed some bugs

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var paperIcon = 'https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/
 var rockIcon = 'https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/stone.png';
 var scissorsIcon = 'http://chittagongit.com/images/rock-paper-scissors-icon/rock-paper-scissors-icon-5.jpg';
 var numberOfGames;
+var total_player_wins = 0;
 var gamesPlayed = 0;
 var cWins = 0;
 var pWins = 0;
 var totalGamesPlayed = 0;
 
 $(document).ready(function(){
-
   $('#start_game').on('click', function(){
     numberOfGames = parseInt($('#games').val());
     if ($('#games').val() == ''){
@@ -18,8 +18,26 @@ $(document).ready(function(){
     $('#start').addClass('hidden')
     $('#game_play_container').slideToggle().removeClass('hidden');
     $('#score_card').slideToggle().removeClass('hidden');
+    setTimeout(() => {
+      $('#restart').removeClass('hidden');
+    }, 1000);
   });
-
+  
+  $('#restart_button').on('click', restartGame)
+  
+  function restartGame(){
+    numberOfGames;
+    cWins = 0;
+    pWins = 0;
+    $('#start').removeClass('hidden')
+    $('#game_play_container').slideToggle().addClass('hidden');
+    $('#score_card').slideToggle().addClass('hidden');
+    $('#restart').addClass('hidden');
+    $('#all_computer_wins').text('');
+    $('#all_user_wins').text('');
+    $('#user_win_rate').text('');
+  };
+  
   $('.game_button').on('click', function(){
     var playerSelection = this.id
     if (playerSelection == 'paper')
@@ -44,35 +62,36 @@ $(document).ready(function(){
       printComputerChoice = paperIcon;
     else printComputerChoice = scissorsIcon;
     $('#computer_play').append(`<img src="${printComputerChoice}" class="played_icons">`)
+    console.log(computerChoice);
     return computerChoice;
   }
 
   function playGame(playerSelection) {
     totalGamesPlayed += 1;
     var computerChoice = computerPlay();
-    if ((playerSelection == computerChoice)) {
-      console.log('draw')
+    if (playerSelection == computerChoice) {
         writeGameResults('draw');
-    } else if (playerSelection == 'rock' && computerChoice == 'paper' || playerSelection == 'scissors' && computerChoice == 'rock' || playerSelection == 'paper' && computerChoice == 'scissors') {
-      console.log('computer')
+    } else if (playerSelection == 'rock' && computerChoice == 'paper' 
+            || playerSelection == 'scissors' && computerChoice == 'rock' 
+            || playerSelection == 'paper' && computerChoice == 'scissors') {
         writeGameResults('computer');
-    } else if (playerSelection == 'rock' && computerChoice == 'scissors' || playerSelection == 'paper' && computerChoice == 'rock' || playerSelection == 'scissors' && computerChoice == 'paper') {
-      console.log('user')
-        writeGameResults('player');
-    }
+      }else writeGameResults('player');
   }
 
   function writeGameResults(winner) {
     if (winner == 'computer'){
-      $('#results').append('<p> Computer Wins, Better luck next time</p>')
+      $('#results').append('<p id="computer_win"> Computer Wins, Better luck next time</p>')
       cWins += 1
       gamesPlayed += 1;
     }else if (winner == 'player'){
-      $('#results').append('<p> You Win, have a beer</p>')
+
+      $('#results').append('<p id="player_win"> You Win, have a beer</p>')
       pWins += 1
       gamesPlayed += 1;
+      total_player_wins += 1
     }else
-      $('#results').append('<p>It\'s a draw. Sad') ;
+      $('#results').append('<p id="draw">It\'s a draw. Sad') ;
+
       writeToTally(cWins, pWins);
       setTimeout(() => {
       (gamesPlayed == numberOfGames) ? gameOver() : null;
@@ -83,14 +102,14 @@ $(document).ready(function(){
   };
 
 function writeToTally(cWins, pWins){
-  var user_win_rate = (pWins / totalGamesPlayed)*100
-
+  var user_win_rate = (total_player_wins / totalGamesPlayed)*100;
   $('#all_computer_wins').text(`Computer Wins: ${cWins}`)
   $('#all_user_wins').text(`User Wins: ${pWins}`)
   user_win_rate == NaN ? $('#user_win_rate').text(`Win Rate: 0%`): $('#user_win_rate').text(`Player winning rate: ${user_win_rate.toFixed(0)}%`);
 }
 
 function gameOver(){
+  restartGame()
   alert("game over")
 }
 
